b301c1fa4bc4512e1adce76a16641390ffc44891
mohamed-bo <medprgrm@gmail.com>
2023-12-19 17:21:39 +0100

    inchalah it works

---
+++ 2-selection_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/**
+ *selection_sort - selection_sort algorithm
+ *@array: array
+ *@size: len of array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, k;
+	int temp;
+
+	if (!array || !size)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		maxIndex = i;
+		for (j = i + 1; j < size; j++)
+			if (array[j] < array[maxIndex])
+				maxIndex = j;
+		if (array[i] > array[maxIndex])
+		{
+			temp = array[i];
+			array[i] = array[maxIndex];
+			array[maxIndex] = temp;
+			print_array(array, size);
+		}
+	}
+}
